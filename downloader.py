b2923cd870bd8e48aa294ed5005d55d87e0a7d9b
Akmal <pandasid05@gmail.com>
2021-07-05 20:48:28 +0800

    pembaruan kode

--- downloader.py
+++ downloader.py
@@ -1,91 +1,102 @@
 '''
 Date: 10-08-2020
 Author: Pandas ID
+Last Update: 05-07-2021
 '''
 
 
-
+# import library
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
 from datetime import datetime
-import requests as req
-import re,os,time
+import requests
+import re
+import os
 
 
+# kode warna
 p = '\033[0m'
 m = '\033[91m'
 h = '\033[92m'
 k = '\033[93m'
 
-class Downloader:
 
-    __chunk = 1024
+class Download:
 
     def __init__(self):
-        self.__url = ''
-        self.__caption = ''
-        self.__video_size = ''
-        self.__video_name = datetime.now().strftime('%m%d%Y%-H%M%S')+'_pandas.mp4'
+        pass
 
-    def banner(self):
-        os.system('clear')
-        print(f'{h}█▀█ ▄▀█ █▄ █ █▀▄ ▄▀█ █▀'.center(72))
-        print(f'█▀▀ █▀█ █ ▀█ █▄▀ █▀█ ▄█{p}'.center(72))
-        print('-----------------------'.center(68))
-        print(f'{h}[{p}FB Video Downloader{h}]{p}'.center(86))
-        print('-----------------------'.center(68))
+    def url(self, url):
+        if re.search(r'www.facebook.com', url):
+            url = url.replace('www', 'mbasic')
+
+        # cek video
+        try:
+            response = requests.get(url).text
+            self.video_url = re.search(r'href\=\"\/video\_redirect\/\?src\=(.*?)\"', response)
+
+            if self.video_url is not None:
+                # mendapatkan caption dari video
+                meta = BeautifulSoup(response, 'html.parser').find('meta', {'property':'og:description'})
+                try:
+                    self.caption = meta['content']
+                except:
+                    self.caption = ''
+
+                self.post_url  = url
+                self.video_url = unquote(self.video_url.group(1))
+
+                print(f'  (!) {h}Video ditemukan...{p}')
+                self.download()
+            else:
+                print(f'  (!) {m}Video tidak ditemukan...{p}')
+        except requests.exceptions.ConnectionError:
+            print(f'   (!) {m}Koneksi error, mohon periksa koneksi internet Anda...{p}')
+        except requests.exceptions.MissingSchema:
+            print(f'  (!) {m}Url tidak valid...{p}')
+
+    def info(self):
+        print()
+        print(f'  (•) {k}Caption:{p} {self.caption}')
+        if self.video_size > 1024:
+            self.video_size = round(self.video_size / 1024)
+            m = 'mb'
+        else:
+            self.video_size = round(self.video_size)
+            m = 'kb'
+        print(f'  (•) {k}Ukuran video:{p} {self.video_size} {m}')
         print()
 
-    # meminta inputan url dari user
-    def setUrl(self):
-        url = input(f'  >{k} url: {p}');print(p)
-        self.__url = url.replace('www', 'mbasic')
-
-    # mengecek url yang dimasukan user jika benar akan mengembalikan url yang telah di parse
-    def cekUrl(self):
-        response = req.get(self.__url).text
-        if 'video_redirect' in response:
-            print(f'  >{h} video found!')
-            self.getVideoInfo()
-            url_video = re.search(r'href\=\"\/video\_redirect\/\?src\=(.*?)\"', response)
-            self.__url = unquote(url_video.group(1)).replace(';', '&')
-        else:
-            exit(f'  >{m} video not found!')
-
-    def getVideoInfo(self):
-        response = req.get(self.__url)
-
-        # mendapatkan caption dari video
-        meta = BeautifulSoup(response.text, 'html.parser').find('meta', {'name':'description'})
-        try:
-            self.__caption= meta['content']
-        except:
-            pass
-
-    def showVideoInfo(self):
-        print(f'\n{p}  > {k}caption:{p} {self.__caption}\n{p}  >{k} video size:{p} {self.__video_size} KB')
 
     def download(self):
-        content = req.get(self.__url)
-        # mendapatkan ukuran video
-        size = round(int(content.headers.get('Content-Length'))/self.__chunk)
-        self.__video_size = size
-        self.showVideoInfo()
-        print(f'\n{p}  >{h} Is downloading...', end='', flush=True)
-        with open('/sdcard/'+self.__video_name, 'wb') as vid:
-            for data in content.iter_content(chunk_size=self.__chunk):
-                vid.write(data)
-        print(f'\n{p}  >{k} videos saved on directory{h} /sdcard/{self.__video_name}')
+        content = requests.get(self.video_url)
+        self.video_size = int(content.headers.get('Content-Length'))/1024
+        video_name = self.video_name
+        try:
+            with open('/sdcard/videos/'+video_name, 'wb') as fl:
+                self.info()
+                print(f'  (!) {h}Mulai mengunduh...{p}')
+                fl.write(content.content)
+            print(f'  (•) {k}Video tersimpan di {h}/sdcard/videos/{video_name}{p}')
+            print(f'\n\n  Kontak:{h} https://t.me/PandasID\n{p}  Blog: {h}https://pandasid.blogspot.com\n')
+        except FileNotFoundError:
+            os.system('mkdir -p /sdcard/videos')
+            self.download()
 
-    def about(self):
-        print(f'\n\n{p}  > My Contact:\n{h}    https://t.me/PandasID\n{p}  > My Blog:\n{h}    https://pandasid.blogspot.com\n\n')
+    @property
+    def video_name(self):
+        return datetime.now().strftime('%m%d%Y%-H%M%S')+'.mp4'
 
 if __name__ == '__main__':
-    # memuat objek dari class "Downloader"
-    downloader = Downloader()
+    os.system('clear')
+    print(f'{h}█▀█ ▄▀█ █▄ █ █▀▄ ▄▀█ █▀'.center(72))
+    print(f'█▀▀ █▀█ █ ▀█ █▄▀ █▀█ ▄█{p}'.center(72))
+    print('-----------------------'.center(68))
+    print(f'{h}[{p}FB Video Downloader{h}]{p}'.center(86))
+    print('-----------------------'.center(68))
+    print()
 
-    downloader.banner()
-    downloader.setUrl()
-    downloader.cekUrl()
-    downloader.download()
-    downloader.about()
+    download = Download()
+
+    url = input(f'  (?) {k}url video: {p}')
+    download.url(url)
